c04458bbe0a61c807aa3ba0c9dad148066633519
sharco home desktop <38721575+sharco-code@users.noreply.github.com>
2020-03-10 23:25:30 +0100

    AjustesController
    creado el controlador del componente ajustes

---
+++ Red-Libros-master/redLibros/src/main/java/controller/AjustesController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class AjustesController{
+
+    @FXML
+    private TextField xTextFieldIP;
+
+    @FXML
+    private TextField xTextFieldPuerto;
+
+    @FXML
+    private TextField xTextFieldUser;
+
+    @FXML
+    private TextField xTextFieldPassword;
+
+    @FXML
+    private ChoiceBox<?> xChoiceBoxFilas;
+
+    @FXML
+    private ChoiceBox<?> xChoiceBoxColumnas;
+
+    @FXML
+    void ApplyCLICKED(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ImportCLICKED(MouseEvent event) {
+
+    }
+
+}
