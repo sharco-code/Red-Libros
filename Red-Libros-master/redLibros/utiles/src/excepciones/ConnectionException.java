97c14b75c278cd8fd9deb7d77cce9e496f083eb2
RaulB0 <48099382+RaulB0@users.noreply.github.com>
2020-03-10 19:33:00 +0100

    utiles hibernate

---
+++ Red-Libros-master/redLibros/utiles/src/excepciones/ConnectionException.java
@@ -0,0 +1,10 @@
+package excepciones;
+
+public class ConnectionException extends RuntimeException {
+	public ConnectionException() {
+		super();
+	}
+	public ConnectionException(String msg) {
+		super(msg);
+	}
+}
---
+++ Red-Libros-master/redLibros/utiles/src/hibernate/UtilesHibernate.java
@@ -0,0 +1,22 @@
+package hibernate;
+
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+public class UtilesHibernate {
+	private static final SessionFactory sessionFactory;
+	static {
+		try {
+			Configuration configuration = new Configuration().configure();
+			sessionFactory = configuration.configure().buildSessionFactory();
+		} catch (Throwable ex) {
+			// TODO: handle exception
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+}
